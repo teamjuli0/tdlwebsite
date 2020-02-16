e8d9db331862e96e2d72a7ac81f8c6e30255a74a
Julio Valdez <valdezjulio95@gmail.com>
2020-02-16 01:31:15 -0800

    fixed minor bug in leaders card view

--- client/src/components/aboutPageCards/leadersCard.js
+++ client/src/components/aboutPageCards/leadersCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { LeadersInfoCard } from "../aboutPageCards";
+import React from 'react'
+import { LeadersInfoCard } from '../aboutPageCards'
 
 import {
   AxelSquare,
@@ -7,61 +7,61 @@ import {
   MikeSquare,
   AdaSquare,
   YadySquare
-} from "../../images/roundIcons";
+} from '../../images/roundIcons'
 
 const leadersCard = () => (
   <>
     <div
-      className="row"
+      className='row'
       style={{
-        paddingTop: "60px"
+        paddingTop: '60px'
       }}
     >
       <LeadersInfoCard
         LeaderImg={AxelSquare}
-        LeaderTitle="PASTOR AUXILIAR"
-        LeaderName="Axel Lopez"
-        LeaderDescription="Porque la paga del pecado es muerte, mas la dádiva de Dios es vida eterna en Cristo Jesús Señor nuestro."
-        Verso="Romanos 6:23"
+        LeaderTitle='PASTOR AUXILIAR'
+        LeaderName='Axel Lopez'
+        LeaderDescription='Porque la paga del pecado es muerte, mas la dádiva de Dios es vida eterna en Cristo Jesús Señor nuestro.'
+        Verso='Romanos 6:23'
       />
       <LeadersInfoCard
         LeaderImg={JulioSquare}
-        LeaderTitle="PASTOR GENERAL"
-        LeaderName="Julio Vanegas"
-        LeaderDescription="Y todo lo que hagáis, hacedlo de corazón, como para el Señor y no para los hombres"
-        Verso="Colosenses 3:23"
+        LeaderTitle='PASTOR GENERAL'
+        LeaderName='Julio Vanegas'
+        LeaderDescription='Y todo lo que hagáis, hacedlo de corazón, como para el Señor y no para los hombres'
+        Verso='Colosenses 3:23'
       />
       <LeadersInfoCard
         LeaderImg={AdaSquare}
-        LeaderTitle="PASTORA GENERAL"
-        LeaderName="Ada Vanegas"
-        LeaderDescription="Dad gracias en todo, porque esta es la voluntad de Dios para con vosotros en Cristo Jesús."
-        Verso="1 Tesalonicenses 5:18"
+        LeaderTitle='PASTORA GENERAL'
+        LeaderName='Ada Vanegas'
+        LeaderDescription='Dad gracias en todo, porque esta es la voluntad de Dios para con vosotros en Cristo Jesús.'
+        Verso='1 Tesalonicenses 5:18'
       />
     </div>
     <div
-      className="row"
+      className='row'
       style={{
-        width: "615px",
-        margin: "auto"
+        maxWidth: '615px',
+        margin: 'auto'
       }}
     >
       <LeadersInfoCard
         LeaderImg={MikeSquare}
-        LeaderTitle="HERMANO"
-        LeaderName="Mike Bayona"
-        LeaderDescription="Jesús le dijo: Yo soy el camino, y la verdad, y la vida; nadie viene al Padre, sino por mí."
-        Verso="Juan 14:6"
+        LeaderTitle='HERMANO'
+        LeaderName='Mike Bayona'
+        LeaderDescription='Jesús le dijo: Yo soy el camino, y la verdad, y la vida; nadie viene al Padre, sino por mí.'
+        Verso='Juan 14:6'
       />
       <LeadersInfoCard
         LeaderImg={YadySquare}
-        LeaderTitle="HERMANA"
-        LeaderName="Yady Bayona"
-        LeaderDescription="Jehová, tú eres mi Dios; te exaltaré, alabaré tu nombre, porque has hecho maravillas"
-        Verso="Isaias 25:1"
+        LeaderTitle='HERMANA'
+        LeaderName='Yady Bayona'
+        LeaderDescription='Jehová, tú eres mi Dios; te exaltaré, alabaré tu nombre, porque has hecho maravillas'
+        Verso='Isaias 25:1'
       />
     </div>
   </>
-);
+)
 
-export default leadersCard;
+export default leadersCard
